f3ed2d1c73882fef12206b67a361fcd1bd580018
DELL <DELL@DESKTOP-RFCV7TH>
2023-12-26 09:53:37 +0530

    String Class Example

---
+++ FirstProject/src/main/java/stringclass/StringExample.java
@@ -0,0 +1,18 @@
+package stringclass;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		String s1="Hello";
+		char ch[]= {'i','n','d','i','a'};
+		String s2=new String(ch);
+		String s3=new String("Example");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
